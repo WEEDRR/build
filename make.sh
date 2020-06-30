46f95384321dd2b42c4fceeecdb7f12d959a7bcb
hyakei <oke123543@gmail.com>
2020-06-30 12:29:28 +0000

    update

--- make.sh
+++ make.sh
@@ -5,8 +5,8 @@ git clone --quiet -j32 https://github.com/WEEDRR/AnyKernel3 -b neesan dapur
 echo "Done"
 TANGGAL=$(date +'%H%M-%d%m%y')
 START=$(date +"%s")
-GCC="/root/tools/gcc-elf/bin/aarch64-elf-"
-GCC32="/root/tools/gcc-arm-elf/bin/arm-eabi-"
+GCC="/root/tools/11/bin/aarch64-elf-"
+GCC32="/root/tools/arm11/bin/arm-eabi-"
 export ARCH=arm64
 export KBUILD_BUILD_USER=Zulf
 export KBUILD_BUILD_HOST=NusantaraDevs
